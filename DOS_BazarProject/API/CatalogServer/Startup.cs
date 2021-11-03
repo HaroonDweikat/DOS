4429b6cee418859f1f04dac5a983b082aededcbd
MahdeSouqi <mahdesouqi@pop-os.localdomain>
2021-11-03 17:45:48 +0200

    add missing import statment

--- DOS_BazarProject/API/CatalogServer/Startup.cs
+++ DOS_BazarProject/API/CatalogServer/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
 
 namespace CatalogServer
 {
